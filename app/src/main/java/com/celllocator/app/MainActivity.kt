98403949af5a40bae2f59d747964907ee76339e2
Fabian Thomys <f.thomys@icloud.com>
2025-01-16 19:50:52 +0100

    feat: implement permission handling for location and phone state
    ### CHANGES
    
    - Added permission checks and requests for location and phone state.
    - Implemented a permission dialog to prompt the user to grant permissions.
    - Updated MainActivityContent to handle permission states and display a message if permissions are not granted.
    - Added a button to open app settings to grant permissions if they were denied.

--- app/src/main/java/com/celllocator/app/MainActivity.kt
+++ app/src/main/java/com/celllocator/app/MainActivity.kt
@@ -1,31 +1,37 @@
 package com.celllocator.app
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CellTower
 import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -38,7 +44,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { MainActivityContent() }
+        setContent {
+            MainActivityContent(
+                checkAndRequestPermissions = { checkAndRequestPermissions() }
+            )
+        }
+    }
+
+    private fun checkAndRequestPermissions(): Boolean {
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_PHONE_STATE
+        )
+
+        val notGranted = permissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (notGranted.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, notGranted.toTypedArray(), 0)
+        }
+
+        return notGranted.isEmpty()
     }
 }
 
@@ -50,80 +78,136 @@ sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon:
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainActivityContent() {
+fun MainActivityContent(checkAndRequestPermissions: () -> Boolean) {
+    val context = LocalContext.current
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     val navController = rememberNavController()
 
     val navItems = listOf(Screen.Cells, Screen.Settings)
+    var showPermissionDialog by remember { mutableStateOf(false) }
+    var permissionsGranted by remember { mutableStateOf(false) }
 
     CellLocatorTheme {
-        Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(
-                            stringResource(R.string.app_name),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
-                    },
-                    scrollBehavior = scrollBehavior,
-                    colors =
-                    topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                        scrolledContainerColor = MaterialTheme.colorScheme.primary,
-                    ),
-                )
-            },
-            content = { padding ->
-                Surface(
-                    modifier = Modifier.padding(padding),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.Cells.route,
-                    ) {
-                        // placeholder for the screens
-                        composable(Screen.Cells.route) {
-                            Text("Info")
-                        }
-                        composable(Screen.Settings.route) {
-                            Text("Settings")
-                        }
-                    }
-                }
-            },
-            bottomBar = {
-                NavigationBar {
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentDestination = navBackStackEntry?.destination
+        LaunchedEffect(Unit) {
+            permissionsGranted = checkAndRequestPermissions()
+            if (!permissionsGranted) {
+                showPermissionDialog = true
+            }
+        }
 
-                    navItems.forEach { screen ->
-                        NavigationBarItem(
-                            icon = {
-                                Icon(
-                                    screen.icon,
-                                    contentDescription = stringResource(id = screen.resourceId)
-                                )
-                            },
-                            label = { Text(stringResource(id = screen.resourceId)) },
-                            selected =
-                            currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                            onClick = {
-                                if (currentDestination?.route != screen.route) {
-                                    navController.navigate(screen.route) {
-                                        popUpTo(navController.graph.startDestinationId)
-                                        launchSingleTop = true
-                                    }
-                                }
-                            },
-                        )
+        if (showPermissionDialog) {
+            AlertDialog(
+                onDismissRequest = {},
+                title = { Text("Permissions Required") },
+                text = { Text("This app requires location and phone state permissions to function properly.") },
+                confirmButton = {
+                    TextButton(onClick = {
+                        permissionsGranted = checkAndRequestPermissions()
+                        showPermissionDialog = !permissionsGranted
+                    }) {
+                        Text("Grant Permissions")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        showPermissionDialog = false
+                    }) {
+                        Text("Cancel")
                     }
                 }
-            },
-        )
+            )
+        }
+
+        if (permissionsGranted) {
+            Scaffold(
+                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                stringResource(R.string.app_name),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+                        },
+                        scrollBehavior = scrollBehavior,
+                        colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                            scrolledContainerColor = MaterialTheme.colorScheme.primary,
+                        ),
+                    )
+                },
+                content = { padding ->
+                    Surface(
+                        modifier = Modifier.padding(padding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screen.Cells.route,
+                        ) {
+                            composable(Screen.Cells.route) {
+                                Text("Info")
+                            }
+                            composable(Screen.Settings.route) {
+                                Text("Settings")
+                            }
+                        }
+                    }
+                },
+                bottomBar = {
+                    NavigationBar {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+
+                        navItems.forEach { screen ->
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        screen.icon,
+                                        contentDescription = stringResource(id = screen.resourceId)
+                                    )
+                                },
+                                label = { Text(stringResource(id = screen.resourceId)) },
+                                selected =
+                                currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                onClick = {
+                                    if (currentDestination?.route != screen.route) {
+                                        navController.navigate(screen.route) {
+                                            popUpTo(navController.graph.startDestinationId)
+                                            launchSingleTop = true
+                                        }
+                                    }
+                                },
+                            )
+                        }
+                    }
+                },
+            )
+        } else {
+            CellLocatorTheme {
+                Scaffold { padding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text("Permissions Required")
+                            Button(onClick = {
+                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                val uri = Uri.fromParts("package", context.packageName, null)
+                                intent.data = uri
+                                context.startActivity(intent)
+                            }) {
+                                Text("Open Settings")
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
